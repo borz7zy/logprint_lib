49267c82c44386b52183665738704b03387c4d69
borz7zy <borz7zy@yandex.ru>
2024-09-20 19:16:53 +0200

    re-fix export define

--- logprint.hxx
+++ logprint.hxx
@@ -1,12 +1,13 @@
 
-#ifndef BP_LIB_EXPORT
+#if defined(BP_LIB_EXPORT)
+#undef BP_LIB_EXPORT
+#endif
 #if defined(_WIN32) || defined(_WIN64)
 #define BP_LIB_EXPORT __declspec(dllexport)
 #elif defined(__linux__) || defined(__APPLE__)
 #define BP_LIB_EXPORT __attribute__((visibility("default")))
 #else
 #define BP_LIB_EXPORT
-#endif
 #endif
 
 extern "C"
